6d7c87933ed95e3e2adf7698d0d20cc828c47619
Rahul Rakshit <rahul.rakshit@posteo.net>
2019-10-07 00:38:14 +0100

    ✨ Create attach_docker_typescript_server

---
+++ attach_docker_typescript_server/src/index.ts
@@ -0,0 +1,15 @@
+import { Response } from "express";
+const express = require("express");
+const cors = require("cors");
+const port = 1357;
+
+const app = express();
+app.use(cors());
+
+app.get("/ping", (_: any, res: Response) => {
+  res.json({ message: "pong" });
+});
+
+app.listen(port, () => {
+  console.log(`🚀 App listening on port ${port}.`);
+});
